34805578ce275c3e2b6aabac013b7e85c06de8b3
Sam Hamm <samhamm@gmail.com>
2017-09-06 12:51:26 -0700

    fixed locations

--- app.js
+++ app.js
@@ -7,31 +7,31 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoic2FtaGFtbSIsImEiOiJjajc5NjNmbzMya2t4MzJwYTZrNWFmbjdoIn0.b8kJbQXAWTBMZEhAxpuYdw'
 }).addTo(mymap);
 
-var rental = L.marker([24.553588, -81.794391]).addTo(mymap);
+const rental = L.marker([24.553588, -81.792197]).addTo(mymap);
 rental.bindPopup("<b>Wendy's rental</b><br>1111 Grinnell").openPopup();
 
-var hemingway = L.marker([24.5512044, -81.8028042]).addTo(mymap);
-hemingway.bindPopup("<b>Hemiingway House</b><br>1111 Grinnell").openPopup();
+const hemingway = L.marker([24.5512044, -81.8006155]).addTo(mymap);
+hemingway.bindPopup("<b>Hemiingway House</b><br>907 Whitehead").openPopup();
 
-var banana = L.marker([24.5488161, -81.79956]).addTo(mymap);
+const banana = L.marker([24.5488161, -81.797366]).addTo(mymap);
 banana.bindPopup("<b>Banana Cafe</b><br>1215 Duval").openPopup();
 
-var publix1 = L.marker([24.5676908, -81.7694085]).addTo(mymap);
+const publix1 = L.marker([24.5659704, -81.7705233]).addTo(mymap);
 publix1.bindPopup("<b>Publix</b><br>1112 Key Plaza").openPopup();
 
-var publix2 = L.marker([24.5694112, -81.7660997]).addTo(mymap);
+const publix2 = L.marker([24.5694112, -81.7639057]).addTo(mymap);
 publix2.bindPopup("<b>Publix</b><br>3316 N Roosevelt Blvd").openPopup();
 
-var bealls = L.marker([24.5626343, -81.777672]).addTo(mymap);
+const bealls = L.marker([24.563204, -81.77625]).addTo(mymap);
 bealls.bindPopup("<b>Beall's Outlet</b><br>2506 N Roosevelt Blvd").openPopup();
 
-var tiger = L.marker([24.5526508, -81.7916624]).addTo(mymap);
+const tiger = L.marker([24.552809, -81.7887]).addTo(mymap);
 tiger.bindPopup("<b>Tiger!</b><br>1300 White").openPopup();
 
-var mallory = L.marker([24.5594225, -81.8096326]).addTo(mymap);
+const mallory = L.marker([24.559027, -81.807014]).addTo(mymap);
 mallory.bindPopup("<b>Mallory Square</b><br>400 Wall").openPopup();
 
-var higgs = L.marker([24.5494199, -81.7821]).addTo(mymap);
+const higgs = L.marker([24.5477945, -81.7870587]).addTo(mymap);
 higgs.bindPopup("<b>Higgs Beach Dog Park</b><br>100 Atlantic Blvd").openPopup();
 
 
@@ -51,7 +51,7 @@ higgs.bindPopup("<b>Higgs Beach Dog Park</b><br>100 Atlantic Blvd").openPopup();
 // circle.bindPopup("I am a circle.");
 // polygon.bindPopup("I am a polygon.");
 
-// var popup = L.popup();
+const popup = L.popup();
 
 function onMapClick(e) {
     popup
